b4e79ea73f0f57cbef01e2d811df604105c08581
Richard Chang <rchang9999@yahoo.com>
2018-06-02 14:09:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,4 @@
 class SongsController < ApplicationController
-  require 'csv'
 
   def index
     @songs = Song.all
@@ -24,7 +23,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    SongsWorker.perform_async(params)
+    SongsWorker.perform_async(params[:file].path)
     redirect_to songs_path
   end
 
